8cf342745015bbc10254e901de01b63729116750
Konrad Rozpadek <sbluek546@gmail.com>
2020-09-03 13:23:17 -0500

    Added comments

--- backend/EmailClient.ts
+++ backend/EmailClient.ts
@@ -1,14 +1,13 @@
 import { createTestAccount, TestAccount, createTransport } from "nodemailer";
 import Mail = require("nodemailer/lib/mailer");
-import smtpTransport = require("nodemailer-smtp-transport");
 import SMTPTransport = require("nodemailer/lib/smtp-transport");
-import { MailOptions } from "nodemailer/lib/json-transport";
+//Get .env enviorment variables
 require("dotenv").config();
-
+//Class to handle sending emails
 export default class EmailClient {
   private _account: TestAccount | null;
   private _transport: Mail;
-
+  //Attempts to create email client and sets ups config to connect with gmail
   constructor() {
     this._account = null;
     createTestAccount().then((account) => {
@@ -20,6 +19,7 @@ export default class EmailClient {
     this._transport = createTransport(
       new SMTPTransport({
         service: "Gmail",
+        //set up .env with EMAIL and EMAILPASS as variables as email login credentials
         auth: {
           user: process.env.EMAIL,
           pass: process.env.EMAILPASS,
@@ -27,9 +27,17 @@ export default class EmailClient {
       })
     );
   }
+  //Returns email account
   get account(): TestAccount | null {
     return this._account;
   }
+  //Sends an email by giving a obj that looks like
+  /*
+    from: <Email Address of send>,
+    to: <Email Address of receiver>,
+    subject: <Subject Text>,
+    text: <Email body>,
+  */
   public send(options: Mail.Options): void {
     this._transport.sendMail(options, (error, info) => {
       if (error) {
--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,11 @@
 import Express from "express";
 import path from "path";
 import indexPage from "./routes/indexPage";
+//Creates express server
 let app = Express();
 require("dotenv").config();
 app.set("views", path.join(__dirname, "views"));
+//Sets html as rendering engine
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 app.use("/", indexPage);
