cae7ccc110d2df316f0d57448885ed5e6aae44ed
Hamziniii <hamzagaziuddin04@gmail.com>
2020-09-09 23:26:45 -0500

    Added Analog and Working Digital clocks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app" :style="info">
-    <div id="middle">
+    <div id="middle" :style="gridStyle">
       <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      <span id="text"><i>Current Class: Adv. Comp. Sci. Topics, Next Class: AP Calc BC</i><br>Time Left: 5:15:03</span>
     </div>
   </div>
 </template>
@@ -18,17 +19,31 @@ import { getColor } from './util/color'
 })
 export default class App extends Vue {
   op = 1
-  colors: [string, string] = this.op == 1 ? ["#667eea", "#764ba2"] : ["#cfd9df", "#e2ebf0"]// This is where you would get custom colors
+  public colors: [string, string] = this.op == 1 ? ["#667eea", "#764ba2"] : ["#cfd9df", "#e2ebf0"]// This is where you would get custom colors
+  gridS = ``
 
   get info() {
     // linear-gradient(to right, orange , yellow, green, cyan, blue, violet)
     // linear-gradient(45deg, ${this.colors[0]} 0%, ${this.colors[1]} 100%)
+    // let list = ["red", "orange", "yellow", "green", "blue", "violet"]
     return `
-      background: linear-gradient(45deg, yellow, green, cyan, blue, violet);
+      background: linear-gradient(45deg, ${this.colors}) repeat;
       color: ${getColor(this.colors[1])};
-      background-size: 300% 300%;
-      animation: AnimationName 60s ease infinite;
+      background-size: 100% 100%;
+      animation: AnimationName 30s ease infinite;
     `
+  }
+
+  get gridStyle() {
+    return this.gridS
+  }
+
+  public beforeMount() {
+    this.gridS = `display: grid; grid-template: ${document.getElementById("middle")?.childElementCount != 1 ? `auto 1fr` : `1fr`} / 1fr; row-gap: 50px`;
+  }
+
+  public mounted() {
+    (this.$el as HTMLElement)?.style.setProperty("--mode", getColor(this.colors[1]) + "b6")
   }
 }
 </script>
@@ -43,6 +58,8 @@ body {
 }
 
 #app {
+  --mode: #fff;
+
   font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -57,11 +74,16 @@ body {
 
   #middle {
     grid-area: 2 / 2;
+    height: 100%;
 
     @keyframes AnimationName {
       0%{background-position:0% 50%}
       50%{background-position:100% 50%}
       100%{background-position:0% 50%}
+    }
+
+    #text {
+      text-align: center;
     }
   }
 }
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="clock">
-    {{ msg }}
     <div id="clock_outer">
-      <div id="clock_inner"></div>
+      <div id="clock_analog_inner" v-if="this.type == 'analog'">
+
+      </div>
+      <div id="clock_digital_inner" v-else>
+        <span id="text">{{this.time.map(t => Math.max(t, 10) == 10 && t != 10 ? "0" + t : t).map((t, i) => i == 0 ? t > 12 ? t - 12 : t : t).join(":")}} {{this.time[0] > 12 ? "PM" : "AM"}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -16,26 +20,66 @@ import { Options, Vue } from 'vue-class-component';
   }
 })
 export default class HelloWorld extends Vue {
+  type: ("analog" | "digital") = "digital"
   msg!: string
+  time: [number, number, number] = [0,0,0]
+  int: number | null = null
+
+  public mounted() {
+    console.log((this.$parent?.$el as HTMLElement).id)
+    this.int = setInterval((() => {const d = new Date(); this.time = [d.getHours(), d.getMinutes(), d.getSeconds()]}).bind(this), 1000)
+  }
+
+  public unmounted() {
+    if(this.int)
+      clearInterval(this.int)
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #clock_outer {
-  width: 100%;
-  height: 100%;
+  --acccent: #764ba2b6;
 
   display: grid;
   grid-template: 1fr auto 1fr / 1fr auto 1fr;
+  min-height: 35.714285714285715vh;
 
-  #clock_inner {
+  #clock_digital_inner {
+    grid-area: 3 / 2;
+    font-size: calc(50vh / 8);
+    display: grid;
+    grid-template: 1fr auto;
+    padding-top: 10px;
+    #text {
+      grid-area: 2 / 1;
+    }
+  }
+
+  #clock_analog_inner { // 
     grid-area: 2 / 2;
+    --size: #{'min(500px, 70vmin)'};
+    --mode: #ffffffb6;
 
-    width: 100px;
-    height: 100px;
-    background: #ffffffd5;
+    width: var(--size);
+    height: var(--size);
+    background: var(--mode);
     border-radius: 50%;
+
+    &:before {
+      --_size: calc(var(--size) * .05);
+      content: '';
+      display: block;
+      position: relative;
+      width: var(--_size);
+      height: var(--_size);
+      left: calc(50% - calc(var(--_size) / 2));
+      top: calc(50% - calc(var(--_size) / 2));
+      background: var(--acccent);
+      border-radius: 50%;
+      z-index: 1000000;
+    }
   }
 }
 </style>
