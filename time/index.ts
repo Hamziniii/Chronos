773c855f92135fa23fece2797d07d56e52f25822
Konrad Rozpadek <sbluek546@gmail.com>
2020-10-01 17:15:00 -0500

    added monday

--- time/index.ts
+++ time/index.ts
@@ -4,16 +4,29 @@ import { DateTime } from "luxon";
 import { EventEmitter } from "events";
 import SchedualManager from "./SchedualManager";
 let nextS = new EventEmitter();
-let settings: SchedualSettings = {
+let monday: SchedualSettings = {
   timeSlots: [
-    { name: "class 1", begin: [4 + 12, 45], end: [4 + 12, 46] },
-    { name: "class 2", begin: [4 + 12, 46], end: [4 + 12, 47] },
+    { name: "Teacher PLC Meetings", begin: [7 + 0, 10], end: [8 + 0, 10] },
+    { name: "Passing Period", begin: [8 + 0, 10], end: [8 + 0, 20] },
+    { name: "0 Hour Intervention", begin: [8 + 0, 20], end: [9 + 0, 0] },
+    { name: "Break", begin: [9 + 0, 0], end: [9 + 0, 30] },
+    { name: "1st Period", begin: [9 + 0, 30], end: [10 + 0, 3] },
+    { name: "Passing Period", begin: [10 + 0, 3], end: [10 + 0, 8] },
+    { name: "2nd Period", begin: [10 + 0, 8], end: [10 + 0, 41] },
+    { name: "Passing Period", begin: [10 + 0, 41], end: [10 + 0, 46] },
+    { name: "3rd Period", begin: [10 + 0, 46], end: [11 + 0, 19] },
+    { name: "Passing Period", begin: [11 + 0, 19], end: [11 + 0, 24] },
+    { name: "4th Period", begin: [11 + 0, 24], end: [11 + 0, 57] },
+    { name: "Passing Period", begin: [11 + 0, 57], end: [0 + 12, 2] },
+    { name: "5th Period", begin: [0 + 12, 2], end: [0 + 12, 35] },
+    { name: "Passing Period", begin: [0 + 12, 35], end: [0 + 12, 40] },
+    { name: "6th Period", begin: [0 + 12, 40], end: [1 + 12, 13] },
+    { name: "Passing Period", begin: [1 + 12, 13], end: [1 + 12, 18] },
+    { name: "7th Period", begin: [1 + 12, 18], end: [1 + 12, 51] },
+    { name: "Passing Period", begin: [1 + 12, 51], end: [1 + 12, 56] },
+    { name: "8th Period", begin: [1 + 12, 56], end: [2 + 12, 26] },
   ],
-  outOfBounds: {
-    name: "out of bounds",
-    begin: [4 + 12, 47],
-    end: [4 + 12, 48],
-  },
+
   tags: ["monday"],
 };
 let settings2: SchedualSettings = {
