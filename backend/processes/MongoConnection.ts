61d49be2b980b9705d28e3813c31ddee0f9edea4
Konrad Rozpadek <sbluek546@gmail.com>
2020-10-08 11:24:39 -0500

    converted to promise

--- backend/processes/MongoConnection.ts
+++ backend/processes/MongoConnection.ts
@@ -3,9 +3,9 @@ import { MongoClient } from "mongodb";
 const uri =
   "mongodb+srv://backend:hamzaIsAMediocreProgrammer@cluster0-97ewx.gcp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-let c;
-let init = async () => {
-  c = await client.connect();
-};
-init();
-export const connection: MongoClient = (c as unknown) as MongoClient;
+
+export const connection: Promise<MongoClient> = client
+  .connect()
+  .then((client) => {
+    return client;
+  });
